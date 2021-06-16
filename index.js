45c44679de193c7b2614f0897823234f89f3dba2
nyanlathotep <pseudobyte@gmail.com>
2021-06-16 16:21:51 -0600

    added buttons to autofill content in field

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 // With enormous thanks to Will Boyd's textarea highlighting example
 // https://codersblock.com/blog/highlight-text-inside-a-textarea/
 
-col_a = [" little girl", " young girl", " little boy", " young boy", " boy ", " little child", " young child", " child ", " kid ", "infant", "preteen", "pre-teen", "shota", " loli ", "toddler", " pedoph", "middle school", "elementary school", "preschool", "high school", "playground", "wee lass", "young lass", "wee lad", "young lad", "primary school", "pre-pubescent", "kindergarten", "young daughter", "young son", "too young", "the baby", " youth ", " youths", / (?:[1-9]|1[0-7]|one|two|three|four|five|six|seven|eight|nine|ten|eleven|twelve|thirteen|fourteen|fifteen|sixteen|seventeen)([ -])years?\1old/gi];
-col_b = [" penis", " dick", " cock", " vagina", " pussy", " clit", " breast", " ass ", " cum", " semen", " sex", "masturbate", " suck", "fuck ", "fucking ", " moan", " throb", " thrust", " tit ", " tits ", "jizz", " anal", " anus ", " hump", " molest", " testicle", "fucks ", " orgasm", " rape ", " raped ", " rapes ", " raping "];
-
+var col_a = [
+  " little girl", " young girl",
+  " little boy", " young boy", " boy ",
+  " little child", " young child", " child ", " kid ",
+  "infant", "preteen", "pre-teen",
+  "shota", " loli ", "toddler", " pedoph",
+  "middle school", "elementary school", "preschool", "high school",
+  "playground",
+  "wee lass", "young lass", "wee lad", "young lad",
+  "primary school", "pre-pubescent", "kindergarten",
+  "young daughter", "young son",
+  "too young",
+  "the baby", " youth ", " youths",
+  / (?:[1-9]|1[0-7]|one|two|three|four|five|six|seven|eight|nine|ten|eleven|twelve|thirteen|fourteen|fifteen|sixteen|seventeen)([ -])years?\1old/gi];
+var col_b = [
+  " penis", " dick", " cock",
+  " vagina", " pussy", " clit",
+  " breast", " ass ",
+  " cum", " semen",
+  " sex", "masturbate", " suck",
+  "fuck ", "fucking ",
+  " moan", " throb",
+  " thrust",
+  " tit ", " tits ",
+  "jizz",
+  " anal", " anus ", " hump", " molest", " testicle",
+  "fucks ", " orgasm", " rape ", " raped ", " rapes ", " raping "];
+var bindings = [
+  {rules: col_a, cls: 'col-a', title: 'Column A (Age-Related)'},
+  {rules: col_b, cls: 'col-b', title: 'Column B (Sex-Related)'}
+];
 // yeah, browser sniffing sucks, but there are browser-specific quirks to handle that are not a matter of feature detection
 var ua = window.navigator.userAgent.toLowerCase();
 var isIE = !!ua.match(/msie|trident\/7|edge/);
@@ -13,11 +41,10 @@ var isIOS = !isWinPhone && !!ua.match(/ipad|iphone|ipod/);
 function applyHighlights(text) {
   text = text
     .replace(/\n$/g, '\n\n');
-    for (rule of col_a) {
-      text = text.replace(new RegExp(rule,'gi'), '<mark class="col-a">$&</mark>');
-    }
-    for (rule of col_b) {
-      text = text.replace(new RegExp(rule,'gi'), '<mark class="col-b">$&</mark>');
+    for (var binding of bindings) {
+      for (var rule of binding.rules) {
+        text = text.replace(new RegExp(rule,'gi'), '<mark class="'+binding.cls+'">$&</mark>');
+      }
     }
   
   if (isIE) {
@@ -56,6 +83,75 @@ function bindEvents() {
     'input': handleInput,
     'scroll': handleScroll
   }); 
+  $('#help').on('click', fill_help);
+  $('#rules').on('click', fill_rules);
+  $('#log').on('click', fill_log);
+}
+
+function fill_text(el, text) {
+  $(el).val(text);
+  handleInput();
+}
+
+var help_text = `Put your text in here to check for filter terms, e.g.:
+You are a high school student in a real analysis class.
+
+Column A terms are blue: infantry, young lady, the babysitter
+Column B terms are orange: cockpit, breastplate, fuck this shit`
+
+function fill_help() {
+  fill_text('#filter-input', help_text);
+}
+
+var rules_intro = `* Note that the spacing matters on literal entries (surrounded and separated by |)
+* For regex rules (surrounded by /), you might want to try a regex testing site like regex101.com to test them in specific
+* All rules are case insensitive
+`
+
+function generate_rules() {
+  var text = [rules_intro];
+  for (var binding of bindings) {
+    text.push(binding.title+':');
+    var plain_rules = [];
+    var re_rules = [];
+    for (var rule of binding.rules) {
+      if (rule instanceof RegExp) {
+        re_rules.push(rule.source);
+      } else {
+        plain_rules.push(rule);
+      }
+    }
+    text.push('|'+plain_rules.join('|')+'|');
+    for (var re of re_rules) {
+      text.push('/'+re+'/');
+    }
+    text.push('');
+  }
+  return text.join('\n');
+}
+
+function fill_rules() {
+  fill_text('#filter-input', generate_rules());
+}
+
+var changelog = [
+  {date: '2021-06-16', items: ['Added buttons to fill help, patterns, and changelog']},
+  {date: '2021-06-15', items: ['Brought implementation up to date with the list on https://rentry.co/vfigb']}
+];
+
+function generate_log() {
+  var lines = [];
+  for (var day of changelog) {
+    lines.push(day.date);
+    for (var item of day.items) {
+      lines.push('* '+item);
+    }
+  }
+  return(lines.join('\n'));
+}
+
+function fill_log() {
+  fill_text('#filter-input', generate_log());
 }
 
 $(function(){
@@ -63,5 +159,5 @@ $(function(){
     fixIOS();
   }
   bindEvents();
-  handleInput();
+  fill_help();
 });
